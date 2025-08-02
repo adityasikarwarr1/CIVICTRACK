d2f4718c57bf6e7e2f746432034b7a92d21d3540
adityasikarwarr1 <adityasikarwarr156@gmail.com>
2025-08-03 03:06:19 +0530

    feat: add issues section with issue tracking cards and related styles

--- MAIN PAGE/page.js
+++
@@ -1,6 +0,0 @@
-// if (localStorage.getItem("visited")) {
-//   window.location.href = "home.html";
-// } else {
-//   localStorage.setItem("visited", "true");
-// }
-// localStorage.removeItem("visited");
