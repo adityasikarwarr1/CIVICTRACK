e7e87662aa97907bfbfc4f0e5293aa5e68c40acc
adityasikarwarr1 <adityasikarwarr156@gmail.com>
2025-08-03 02:12:38 +0530

    feat: add admin section with officials' profiles and update navigation links

---
+++ MAIN PAGE/page.js
@@ -0,0 +1,6 @@
+// if (localStorage.getItem("visited")) {
+//   window.location.href = "home.html";
+// } else {
+//   localStorage.setItem("visited", "true");
+// }
+// localStorage.removeItem("visited");
